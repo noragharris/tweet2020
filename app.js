150848b89feb0e5dedb8c6de6cdbdb88c2ef2779
Nora Harris <nora.harris@climate.com>
2019-12-04 11:53:34 -0600

    switched from http to express

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const http = require('http');
+const express = require('express')
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = express()
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+//Index Route
+app.get('/', (req, res) => {
+  res.send('index')
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const port = 3000
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+}) 
